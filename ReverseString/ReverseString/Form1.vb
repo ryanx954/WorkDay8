f264cf55a255984617f1cc1310250812559e3da6
idsc3101 <idsc3101@gmail.com>
2014-10-13 17:09:01 -0500

    10/13/2014

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -4,9 +4,18 @@ Public Class Form1
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         Dim st As String = txtInput.Text
+        Dim i As Integer
+        Dim result As String = ""
+      
 
-        ' Please write your code here!
+        For i = st.Length - 1 To 0 Step -1
+            result += st.Chars(i)
+        Next
 
-        txtOutput.Text = "Not implemented yet!"
+        txtOutput.Text = result
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
